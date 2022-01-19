6f0e8d37336e388d8456221c483f87392553dec7
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2022-01-19 18:43:43 -0500

    Building a Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,17 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+   if (props.danger) {
+      buttonClass += " button--danger";
+    }
+
+   return <button  
+   className={buttonClass}
+   onClick={props.onClick}
+   disabled={props.disabled}>{props.children}</button>;
 }
--- stories/index.js
+++ stories/index.js
@@ -14,11 +14,13 @@ storiesOf("Button", module)
   .add("Base", () => <Button>Base</Button>)
   .add("Confirm", () => <Button confirm>Confirm</Button>)
   .add("Danger", () => <Button danger>Cancel</Button>)
-  .add("Clickable", () => (
-    <Button onClick={action("button-clicked")}>Clickable</Button>
-  ))
-  .add("Disabled", () => (
-    <Button disabled onClick={action("button-clicked")}>
-      Disabled
-    </Button>
+  .add("Clickable", () => (<Button onClick={action("button-clicked")}>Clickable</Button>))
+  .add("Disabled", () => (<Button disabled onClick={action("button-clicked")}>Disabled</Button>
+  // .add("Clickable", () => (
+  //   <Button onClick={action("button-clicked")}>Clickable</Button>
+  // ))
+  // .add("Disabled", () => (
+  //   <Button disabled onClick={action("button-clicked")}>
+  //     Disabled
+  //   </Button>
   ));
