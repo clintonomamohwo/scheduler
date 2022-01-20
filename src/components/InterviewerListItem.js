964be7a095f0d7088d5ea35e43af47db045ae426
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2022-01-20 11:04:08 -0500

    Building the Interviewer Item

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
 export default function InterviwerListItem(props) {
   console.log("ILI: ", props)
 
+  let interviewClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
   return (
 
-<li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
-  );
+<li className={interviewClass} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>)
 
 }
