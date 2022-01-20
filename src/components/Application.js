4207e08a1940c3d266d44d1117bf6b1b629cbd1f
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2022-01-20 00:10:47 -0500

    Updating our App

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,24 @@
 import React from "react";
+import DayList from "./DayList";
+import "components/Application.scss"; 
 
-import "components/Application.scss";
-
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 export default function Application(props) {
   return (
     <main className="layout">
@@ -13,7 +30,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={"Monday"}
+            setDay={(day) => console.log(day)}
+          />{" "}
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
