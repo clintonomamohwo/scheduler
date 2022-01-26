7d6fbe7996844dfef7274a1ef478498a3ef763a5
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2022-01-25 22:00:38 -0500

    Fixed Creating Appointment to show Created Appointments

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -46,7 +46,7 @@ export default function Empty(props) {
     <section className="appointment__card-right">
       <section className="appointment__actions">
         <Button danger onClick={props.onCancel}>Cancel</Button>
-        <Button confirm onClick={props.onSave}>Save</Button>
+        <Button confirm onClick={()=>props.onSave(name,interviewer)}>Save</Button>
       </section>
     </section>
   </main>
